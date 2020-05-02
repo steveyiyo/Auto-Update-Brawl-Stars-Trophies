587f2a1685db4111535b84a514b07ab91988e026
SteveYi Yo <info@steveyi.net>
2020-05-03 01:08:49 +0800

    optimization code

--- trophies.php
+++ trophies.php
@@ -23,34 +23,53 @@ curl_close($ch);
 if (isset($data["reason"])) {
   $errormsg = true;
 }
+if ($data["reason"] == 'notFound') {
+  $notfound = true;
+}
+if ($data["reason"] == 'inMaintenance') {
+  $maintain = true;
+}
 ?>
 <!DOCTYPE html>
 <html>
 <head>
   <meta charset="UTF-8">
-  <title>
-    <?php 
+  <title><?php 
     if (isset($errormsg)) {
       echo '即時獎杯系統' ;
     } else{
       echo $data["name"];
     }
-    ?>
-  </title>
+    ?></title>
   <link rel="shortcut icon" href="img/icon_trophy.png" type="image/x-icon">
-  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@900&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@500;900&display=swap" rel="stylesheet">
   <link rel="stylesheet" type="text/css"  href="css/style.css">
-  <meta http-equiv="refresh" content="5" />
+  <?php
+    if (isset($errormsg)) {
+    }
+    else { echo "<meta http-equiv='refresh' content='5' />\n";
+    }
+  ?>
 </head>
-    
 <body>
 <?php
   if (isset($errormsg)) {
-    echo "<a>", '錯誤訊息: 請檢查你的玩家標籤是否錯誤(或是官方正在維護)' , "</a>";
-  } else{
-    echo "<h1>", $data["trophies"] , "</h1>";
+    echo "<form>\n",
+    "<label for='tag'>",'玩家標籤:',"</label>\n",
+    "<input type='tag' name='tag' id='tag' required/>\n",
+    "<button type='submit' class='btn'>",'送出', "</button>", "<p>\n";
+    if (isset($maintain)) {
+      echo "<a>",'官方正在維護',"</a>\n";
+    } 
+    else if (empty($playertag)) {
+      echo "<a>",'請輸入玩家標籤',"</a>\n";
+    }
+    else if (isset($notfound)) {
+      echo "<a>",'你輸入的玩家標籤錯誤',"</a>\n";
+    }
+  } else {
+    echo "<h1>",$data["trophies"],"</h1>\n";
   }
 ?>
-
 </body>
 </html>
